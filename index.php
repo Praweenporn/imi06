01539f4b25228430ddd379a8eecfbb2ebd418f46
Praweenporn <71056576+Praweenporn@users.noreply.github.com>
2021-08-18 02:21:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -31,12 +31,10 @@
 
 <?php
 
-$humidity = file_get_contents('thingspeak.com/channels/1458414/fields/1/last.txt');
-  /*thingspeak.com/channels/1458414/widgets/348521
-  thingspeak.com/channels/1458414/fields/1/last.txt
-  */
+$humidity = file_get_contents('https://api.thingspeak.com/channels/1458414/fields/1/last.txt');
 $temperature  = file_get_contents('https://api.thingspeak.com/channels/1458414/fields/2/last.txt');
 
 ?>
+<span id="displayValue" style="outline: rgb(112, 128, 144) solid 0.5px;">27.70</span>
  <div1> Hum: <?php echo $humidity?> %</div1>
  <div2> Temp: <?php echo $temperature?> C</div2>
