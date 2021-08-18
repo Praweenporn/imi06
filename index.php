4990345b0884df388ca894f68a69fb1d7f335a59
Praweenporn <71056576+Praweenporn@users.noreply.github.com>
2021-08-18 15:04:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
 <script type="text/JavaScript">
   function timedRefresh(timeoutPeriod) {
   setTimeout("location.reload(true);",timeoutPeriod);
-  }
+ }
+  function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
+  setTimeout(startTime, 1000);
+ }
+  function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
 </script>
+
 <style type="text/css">
   body { background-color: #ffffff; }
   div1 {
@@ -28,8 +43,9 @@
 }
 </style>
 
-<body onload="JavaScript:timedRefresh(14000);">
+<body onload="JavaScript:timedRefresh(14000);",onload="startTime()">
 <br><h1 align="center">Monitor Temperature & Humidity: Prweenporn Mathurot </h1></br>
+
 <table align="center">
 <tr algin="center">
  <td align="center"><iframe width="420" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/1458414/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe></td>
@@ -47,4 +63,4 @@ $temperature  = file_get_contents('https://api.thingspeak.com/channels/1458414/f
 ?>
  <div1> Hum: <?php echo $humidity?> %</div1>
  <div2> Temp: <?php echo $temperature?> C</div2>
- <div3> <a href="https://goo.gl/maps/LPdPmFXEhwc5VgXw9"> Location: Phang nga</a></div3>
+ <div3> <a href="https://goo.gl/maps/LPdPmFXEhwc5VgXw9" target="_blank"> Location: Phang nga</a></div3>
