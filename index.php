9cc0442e9974b85f621892a106934080f7d27763
Praweenporn <71056576+Praweenporn@users.noreply.github.com>
2021-08-18 14:25:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   background-color: lightblue;   
   position: relative;
   font-size: 30px;
-  left: 120px;
+  left: 150px;
   top: 50px;
  }
   div2 {
@@ -19,11 +19,17 @@
   left: 380px;
   top: 50px;
 }
+  div3 {
+  background-color: lightblue; 
+  position: relative;
+  font-size : 30px;
+  left: 650px;
+  top: 50px;
+}
 </style>
 
-<body onload="JavaScript:timedRefresh(15000);">
-<h1> Prweenporn Mathurot </h1>
-<h1 align="center">Monitor temperature & humidity </h1>
+<body onload="JavaScript:timedRefresh(14000);">
+<h1 align="center">Monitor Temperature & Humidity: Prweenporn Mathurot </h1>
 <table align="center">
 <tr algin="center">
  <td align="center"><iframe width="420" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/1458414/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe></td>
@@ -38,7 +44,8 @@
 
 $humidity = file_get_contents('https://api.thingspeak.com/channels/1458414/fields/1/last.txt');
 $temperature  = file_get_contents('https://api.thingspeak.com/channels/1458414/fields/2/last.txt');
-
+$location  = file_get_contents('https://api.thingspeak.com/channels/1458414/maps/channel_show/last.txt');
 ?>
  <div1> Hum: <?php echo $humidity?> %</div1>
  <div2> Temp: <?php echo $temperature?> C</div2>
+ <div2> Location: <?php echo $location?> C</div2>
