65096b1f14e866c6f81f406f86fe8de1e5217983
Praweenporn <71056576+Praweenporn@users.noreply.github.com>
2021-08-18 14:27:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -23,13 +23,13 @@
   background-color: lightblue; 
   position: relative;
   font-size : 30px;
-  left: 650px;
+  left: 800px;
   top: 50px;
 }
 </style>
 
 <body onload="JavaScript:timedRefresh(14000);">
-<h1 align="center">Monitor Temperature & Humidity: Prweenporn Mathurot </h1>
+<br><h1 align="center">Monitor Temperature & Humidity: Prweenporn Mathurot </h1></br>
 <table align="center">
 <tr algin="center">
  <td align="center"><iframe width="420" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/1458414/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe></td>
@@ -48,4 +48,4 @@ $location  = file_get_contents('https://api.thingspeak.com/channels/1458414/maps
 ?>
  <div1> Hum: <?php echo $humidity?> %</div1>
  <div2> Temp: <?php echo $temperature?> C</div2>
- <div2> Location: <?php echo $location?> C</div2>
+ <div2> Location: <?php echo $location?> </div2>
